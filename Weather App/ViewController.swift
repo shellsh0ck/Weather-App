1190c71e73b2f2f528b17be7f17095c173dd6200
shellsh0ck <shellshock@fastmail.fm>
2014-10-01 15:06:44 +0600

    Initial Release
    Version: 0.0

---
+++ Weather App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather App
+//
+//  Created by Vitaly Sulimov on 2014-10-01.
+//  Copyright (c) 2014 Vitaly Sulimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
