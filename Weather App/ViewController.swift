db82d2bf5c7c1256e2b493dede9112d81cafa022
shellsh0ck <shellshock@fastmail.fm>
2014-10-01 16:43:43 +0600

    Snapshot [M1]
    [+] API Key
    [+] URL constructor
    [+] Receive and save data from web

--- Weather App/ViewController.swift
+++ Weather App/ViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let apiKey = "4f85c37e7bdc7fc1af8cbd49f1e501b2"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let baseURL = NSURL(string: "https://api.forecast.io/forecast/\(apiKey)/")
+        let forecastURL = NSURL(string: "37.8267,-122.423", relativeToURL: baseURL)
+        
+        let weatherData = NSData.dataWithContentsOfURL(forecastURL, options: nil, error: nil)
+        println(weatherData)
     }
 
     override func didReceiveMemoryWarning() {
